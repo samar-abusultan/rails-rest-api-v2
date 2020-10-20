94bc8f494a80c5da9dba7a78f6f6111593783436
Samar <samar.abusultan@hungerstation.com>
2020-10-20 14:50:18 +0300

    feat(articles): add rest api methods

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,45 @@
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+
+      def index
+        articles = Article.all
+        render json: {status: 'Success', message: 'Loaded articles', data: articles}, status: :ok
+      end
+
+      def show
+        article = Article.find(params[:id])
+        render json: {status: 'Success', message: 'Loaded article', data: article}, status: :ok
+      end
+
+      def create
+        article = Article.new(article_params)
+        if article.save
+          render json: {status: 'Success', message: 'new article has been created', data: article}, status: :ok
+        else
+          render json: {status: 'Error', message: 'article has not been saved', data: article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        article = Article.find(params[:id])
+        article.destroy
+        render json: {status: 'Success', message: 'article has been removed', data: article}, status: :ok
+      end
+
+      def update
+        article = Article.find(params[:id])
+        if article.update_attributes(article_params)
+          render json: {status: 'Success', message: 'article has been updated', data: article}, status: :ok
+        else
+          render json: {status: 'Error', message: 'article has not been updated', data: article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def article_params
+        params.permit(:title, :body)
+      end
+    end
+  end
+end
